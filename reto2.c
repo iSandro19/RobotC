1246f6f0e6228778223f94e33191204cddf0ec45
Óscar Alejandro Manteiga Seoane <oscar.manteiga@udc.es>
2023-02-11 22:41:27 +0100

    Update reto2.c

--- reto2.c
+++ reto2.c
@@ -1,7 +1,61 @@
+//#### RobotC File ####//
+// Robótica Q8 2022/2023
+// Title: Reto 2
+// Authors:
+//   - Óscar Alejandro Manteiga Seoane
+//   - Antonio Vila Leis
 
-task main()
-{
+// Function definition
+void forward(speed) {
+    setMotorSpeed(motorB, speed);
+    setMotorSpeed(motorC, speed);
+    return;
+}
 
+void reverse(speed) {
+    setMotorSpeed(motorB, -speed);
+    setMotorSpeed(motorC, -speed);
+    return;
+}
 
+void stop() {
+    setMotorSpeed(motorB, 0);
+    setMotorSpeed(motorC, 0);
+    return;
+}
 
+// Main task
+task main() {
+    // Loop
+    while(true) {
+        clearDebugStream();
+
+        // While distance > 10, go forward
+        while(getUSDistance(S4) > 10) {
+            // Forward
+            forward(SensorValue[S4]);
+
+            // Print SensorValue only if it change
+            if(getUSDistance(S4) != SensorValue[S4]) {
+                writeDebugStreamLine("Distance: %d", SensorValue[S4]);
+            }
+        }
+
+        // Stop and red led
+        stop();
+
+        // While distance > 10, go forward
+        while(getUSDistance(S4) > 20) {
+            // Reverse
+            reverse(SensorValue[S4]);
+
+            // Print SensorValue only if it change
+            if(getUSDistance(S4) != SensorValue[S4]) {
+                writeDebugStreamLine("Distance: %d", SensorValue[S4]);
+            }
+        }
+
+        stop();
+        stopAllTasks();
+    }
 }
