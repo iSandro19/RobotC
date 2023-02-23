0ab4bbed3f8066d7a6158ed8174c89db463fd150
Óscar Alejandro Manteiga Seoane <manteigaseoaneoscar@gmail.com>
2023-02-23 11:47:16 +0100

    Update paredes.c

--- paredes.c
+++ paredes.c
@@ -7,9 +7,16 @@
 
 ///////////////////////////////// Definitions
 const int CORNER_ANGLE = 90;
-const int ADJUST_ANGLE = 10;
+const int ADJUST_ANGLE = 15;
 const int MAX_DISTANCE = 30;
 const int MIN_DISTANCE = 15;
+
+// Stop
+void stop() {
+	setMotorSpeed(motorB, 0);
+	setMotorSpeed(motorC, 0);
+	return;
+}
 
 // Forward
 void forward(int speed) {
@@ -32,6 +39,7 @@ void left(int degrees) {
 		setMotorSpeed(motorB, -20);
 		setMotorSpeed(motorC, 20);
 	}
+	stop();
 	return;
 }
 
@@ -42,12 +50,50 @@ void right(int degrees) {
 		setMotorSpeed(motorB, 20);
 		setMotorSpeed(motorC, -20);
 	}
+	stop();
 	return;
 }
 
+// Perpendicular
 void perpendicular() {
-	// Check if the robot is perpendicular to the detected wall in front
-	// If it is no, position it perpendicular to the wall
+	// If robot is perpendicular, then its distance is smaller than small left turn distance and small right turn distante
+	// If it is not perpendicular, move to the position
+	int front_distance = 0;
+	int left_distante = 0;
+	int right_distante = 0;
+
+	// Check front wall
+	front_distance = getUSDistance(S4);
+	writeDebugStreamLine("Front: %d", front_distance);
+
+	// Check left wall
+	left(ADJUST_ANGLE);
+	left_distante = getUSDistance(S4);
+	writeDebugStreamLine("Left: %d", left_distante);
+	right(ADJUST_ANGLE);
+
+	// Check right wall
+	right(ADJUST_ANGLE);
+	right_distante = getUSDistance(S4);
+	writeDebugStreamLine("Right: %d", right_distante);
+	left(ADJUST_ANGLE);
+
+	if(front_distance == left_distante &&
+	   front_distance == right_distante) {
+		writeDebugStreamLine("It is perpendicular");
+		return;
+	}
+
+	// If robot is not perpendicular, move to the position
+	if(left_distante < front_distance) {
+		writeDebugStreamLine("Adjust a little bit to the left");
+		left(ADJUST_ANGLE);
+	} else if (right_distante < front_distance){
+		writeDebugStreamLine("Adjust a little bit to the right");
+		right(ADJUST_ANGLE);
+	}
+
+	return;
 }
 
 // Check right wall
@@ -67,13 +113,6 @@ bool check_wall() {
 		return false;
 	}
 }
-
-// Stop
-void stop() {
-    setMotorSpeed(motorB, 0);
-    setMotorSpeed(motorC, 0);
-    return;
-}
 /////////////////////////////////
 
 ///////////////////////////////// Main task
@@ -87,7 +126,7 @@ task main() {
 		perpendicular();
 
 		if(right_wall) {
-			left(CORNER_ANGLE-5);
+			left(CORNER_ANGLE-4);
 			clearTimer(T1);
 			while(time1[T1] < 1000) {
 				forward(50);
