85a518af9b8b8eec3ae5fc2e9f7ea0c8e0ded151
Óscar Alejandro Manteiga Seoane <manteigaseoaneoscar@gmail.com>
2023-03-14 08:28:37 +0100

    Update repo

--- practica.c
+++ practica.c
@@ -7,7 +7,6 @@
 
 // Definitions ################################################################
 const int CORNER_ANGLE = 90;
-const int ADJUST_ANGLE = 5;
 const int MAX_DISTANCE = 30;
 const int MIN_DISTANCE = 5;
 
@@ -24,7 +23,6 @@ task escape() {
 		if(getTouchValue(S1) == 1 || currentDistance < MIN_DISTANCE) {
 			semaphoreLock(semaphore12);
 			if(bDoesTaskOwnSemaphore(semaphore12)) {
-				writeDebugStreamLine("ESCAPE\n");
 				setMotorSpeed(motorB, -50);
 				setMotorSpeed(motorC, -50);
 				sleep(500);
@@ -56,13 +54,11 @@ task light() {
 
 			if(luz > luzUmbral) {
 				semaphoreLock(semaphore23);
-				writeDebugStreamLine("LIGHT 2\n");
 				setMotorSpeed(motorB, 30);
 				setMotorSpeed(motorC, -30);
 				while(luz <= getColorAmbient(S3)) {
 					luz = getColorAmbient(S3);
 				}
-				writeDebugStreamLine("LIGHT 3\n");
 				setMotorSpeed(motorB, -30);
 				setMotorSpeed(motorC, 30);
 				while(luz <= getColorAmbient(S3)) {
@@ -81,20 +77,19 @@ task light() {
 
 task follow_wall() {
 	bool pared = false;
-	
+
 	while(true) {
 		int currentDistance = getUSDistance(S4);
 
 		semaphoreLock(semaphore23);
-		if (bDoesTaskOwnSemaphore(semaphore23)) { //Si no estamos sigiuiendo luz
-            writeDebugStreamLine("Seguir pared");
+		if (bDoesTaskOwnSemaphore(semaphore23)) {
 
 			if(bDoesTaskOwnSemaphore(semaphore34)) {
 				semaphoreUnlock(semaphore34);
-			} //Si siguiendo pared: dejar seguir
+			}
 
 			if (currentDistance < 25){
-				semaphoreLock(semaphore34); //Seguir pared
+				semaphoreLock(semaphore34);
 					pared = true;
 					resetGyro(S2);
 					repeatUntil(getGyroDegrees(S2) <= -110) {
@@ -102,24 +97,24 @@ task follow_wall() {
 						setMotorSpeed(motorC, 25);
 					}
 					resetGyro(S2);
-				semaphoreUnlock(semaphore34); //Dejar seguir pared
+				semaphoreUnlock(semaphore34);
 			}
 
 			if(currentDistance > 25 && pared) {
-				semaphoreLock(semaphore34); //Seguir pared
+				semaphoreLock(semaphore34);
 					resetGyro(S2);
 					repeatUntil(getGyroDegrees(S2) >= 5) {
 						setMotorSpeed(motorB, 60);
 						setMotorSpeed(motorC, 30);
 					}
 					resetGyro(S2);
-				semaphoreUnlock(semaphore34); //Dejar seguir pared
+				semaphoreUnlock(semaphore34);
 			}
-			semaphoreUnlock(semaphore23); //Dejar libre seguir luz
+			semaphoreUnlock(semaphore23);
 		} else {
 			if(!bDoesTaskOwnSemaphore(semaphore34)) {
 				semaphoreLock(semaphore34);
-			} //Si no siguiendo pared: seguir pared
+			}
 		}
 	}
 }
@@ -131,11 +126,9 @@ task go_to_wall() {
 		semaphoreLock(semaphore34);
 		if(bDoesTaskOwnSemaphore(semaphore34)) {
 			if (currentDistance > MAX_DISTANCE) {
-				writeDebugStreamLine("GO TO WALL - 100\n");
 				setMotorSpeed(motorB, 100);
 				setMotorSpeed(motorC, 100);
 			} else {
-				writeDebugStreamLine("GO TO WALL - Distance\n");
 				setMotorSpeed(motorB, currentDistance);
 				setMotorSpeed(motorC, currentDistance);
 			}
@@ -146,7 +139,6 @@ task go_to_wall() {
 
 // Main #######################################################################
 task main() {
-    // Clear debug stream window and wait 1s
     clearDebugStream();
 	setMotorSpeed(motorA, 100);
     sleep(500);
@@ -155,19 +147,15 @@ task main() {
 	luz = getColorAmbient(S3);
 	sleep(500);
 	luzUmbral = luz*1.8;
-
-	// Initialize semaphores
     semaphoreInitialize(semaphore12);
 	semaphoreInitialize(semaphore23);
 	semaphoreInitialize(semaphore34);
 
-	// Initialize tasks
 	startTask(escape);
 	startTask(light);
 	startTask(follow_wall);
 	startTask(go_to_wall);
 
-    // Bucle inicial
     while(true) {
 		luz = getColorName(S3);
 		if(luz > luzUmbral*1.6) {
